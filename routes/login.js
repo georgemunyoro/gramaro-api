0e2c2e6b7432c1828e7565b259db771793127e9e
George Munyoro <32019551+georgemunyoro@users.noreply.github.com>
2020-10-12 22:46:12 +0200

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -10,19 +10,19 @@ const router = express.Router();
 
 router.post("/", async (req, res) => {
   const { email, password } = req.body;
-  if (email == null || password == null) {
+  if (email === null || password === null) {
 	handleEmptyData(req, res);
   } else {
 	try {
 	  const databaseConnection = getDatabaseConnection();
 	  const { rows: user } = await databaseConnection.query(`SELECT ID, USERNAME, PASSWORD FROM USERS WHERE EMAIL='${email}'`);
 
-	  if (user.length == 0) {
+	  if (user.length === 0) {
 		res.json({ message: "Could not find user" });
 		return
 	  }
 
-	  if (user[0].password != password) {
+	  if (user[0].password !== password) {
 		res.json({ message: "Incorrect password" });
 		return
 	  }
