eaf72cc084a606fdf41943ac6b9c65eab03cb2be
George Munyoro <george.guva@outlook.com>
2021-02-09 20:22:45 +0200

    refactor(routes): use es6 imports

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,16 @@
 import Express from "express";
-import loginRouter from "./login";
+// import loginRouter from "./login";
 import notesRouter from "./notes";
-import signupRouter from "./signup";
-import usersRouter from "./user";
+// import signupRouter from "./signup";
+// import usersRouter from "./user";
 import { version } from "../../package.json";
 
 const rootRouter = Express.Router();
 
-rootRouter.use("/signup", signupRouter);
-rootRouter.use("/login", loginRouter);
-rootRouter.use("/user", usersRouter);
-rootRouter.use("/notes", notesRouter);
+// rootRouter.use("/signup", signupRouter);
+// rootRouter.use("/login", loginRouter);
+// rootRouter.use("/user", usersRouter);
+rootRouter.use("/note", notesRouter);
 
 rootRouter.get("/", (req, res) => {
   res.status(200).json({
